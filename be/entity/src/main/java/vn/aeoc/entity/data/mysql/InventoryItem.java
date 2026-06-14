ecf8cba991612b531c2ea509c5186732d429411e
NTQD <vodaimay@gmail.com>
2026-06-14 10:59:32 +0700

    Fix inventory item field mapping to database

--- be/entity/src/main/java/vn/aeoc/entity/data/mysql/InventoryItem.java
+++ be/entity/src/main/java/vn/aeoc/entity/data/mysql/InventoryItem.java
@@ -12,9 +12,25 @@ public class InventoryItem {
     private Integer id;
     private String name;
     private String description;
-    private BigDecimal price;
-    private Integer stock;
+    private BigDecimal sellingPrice;
+    private Integer stockQuantity;
     private String imageUrl;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public BigDecimal getPrice() {
+        return sellingPrice;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.sellingPrice = price;
+    }
+
+    public Integer getStock() {
+        return stockQuantity;
+    }
+
+    public void setStock(Integer stock) {
+        this.stockQuantity = stock;
+    }
 }
